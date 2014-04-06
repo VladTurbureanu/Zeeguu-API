619cc087ce1c8143f53c6f38fdb56f59a640c14f
Mircea Lungu <mircea.lungu@gmail.com>
2014-04-06 13:44:44 +0200

    added 1,2,7,30 days flash cards strategy. combined with word ranking

--- zeeguu/gym/views.py
+++ zeeguu/gym/views.py
@@ -1,11 +1,11 @@
-# -*- coding: utf8 -*-
+from datetime import datetime, timedelta, date
 import json
 
 import flask
+from numpy.lib.type_check import datetime_data
 
 from zeeguu import model
 import sys
-import zeeguu
 
 
 gym = flask.Blueprint("gym", __name__)
@@ -66,6 +66,10 @@ def contributions():
         return flask.redirect(flask.url_for("gym.login"))
 
     contribs,dates = flask.g.user.contribs_by_date()
+    # contribs_by_url = {}
+    # for contrib in contribs:
+    #     contribs_by_url.setdefault(contrib.text.url, []).append(contrib)
+
     urls_by_date = {}
     contribs_by_url = {}
     for date in dates:
@@ -89,6 +93,23 @@ def recommended_texts():
     if not flask.g.user:
         return flask.redirect(flask.url_for("gym.login"))
 
+    # contribs,dates = flask.g.user.contribs_by_date()
+    # contribs_by_url = {}
+    # for contrib in contribs:
+    #     contribs_by_url.setdefault(contrib.text.url, []).append(contrib)
+
+    # urls_by_date = {}
+    # contribs_by_url = {}
+    # for date in dates:
+    #     sys.stderr.write(str(date)+"\n")
+    #     for contrib in contribs[date]:
+    #         urls_by_date.setdefault(date, set()).add(contrib.text.url)
+    #         contribs_by_url.setdefault(contrib.text.url,[]).append(contrib)
+    # sys.stderr.write(str(urls_by_date)+"\n")
+    # sys.stderr.write(str(contribs_by_url)+"\n")
+
+
+
     return flask.render_template("recommended_texts.html",
                                  # contribs_by_url=contribs_by_url,
                                  # urls_by_date=urls_by_date,
@@ -104,45 +125,67 @@ def flash_cards():
     return flask.render_template("flash_cards.html", languages=languages)
 
 
+def redisplay_card_simple(cards):
+    cards.sort(key=lambda x: x.last_seen)
+    card = cards[1]
+    return card
 
-def get_next_card(user, from_lang, to_lang):
+
+def redisplay_card_aware_of_days(cards):
+
+    interesting_intervals = [1,2,7,31]
+    interesting_dates = [date.today() - timedelta(days=x) for x in interesting_intervals]
+
+
+    interesting_cards = [card for card in cards if card.last_seen.date() in interesting_dates]
+    interesting_cards.sort(key=lambda card: card.contribution.origin.word_rank)
+
+    if interesting_cards:
+        card = interesting_cards[0]
+        card.set_reason("seen on: " + card.last_seen.strftime("%d/%m/%y") + " rank: " + str(card.contribution.origin.word_rank))
+        return card
+
+    cards_not_seen_today = [card for card in cards if card.last_seen.date() != date.today()]
+    cards_not_seen_today.sort(key=lambda card: card.contribution.origin.word_rank)
+
+    if cards_not_seen_today:
+        card = cards_not_seen_today[0]
+        card.set_reason("seen on: " + card.last_seen.strftime("%d/%m/%y") + " rank: " + str(card.contribution.origin.word_rank))
+        return card
+
+    return None
+
+
+
+
+@gym.route("/gym/question/<from_lang>/<to_lang>")
+def question(from_lang, to_lang):
     if not flask.g.user:
         return flask.abort(401)
 
     from_lang = model.Language.find(from_lang)
     to_lang = model.Language.find(to_lang)
 
-    contributions_never_tested = (
+    contributions = (
         model.Contribution.query.filter_by(user=flask.g.user)
                                 .join(model.Word, model.Contribution.origin)
                                 .join(model.WordAlias,
                                       model.Contribution.translation)
     )
-    forward = contributions_never_tested.filter(
+    forward = contributions.filter(
         model.Word.language == from_lang,
         model.WordAlias.language == to_lang
     )
-    backward = contributions_never_tested.filter(
+    backward = contributions.filter(
         model.Word.language == to_lang,
         model.WordAlias.language == from_lang
     )
-    # zeeguu.app.logger.debug(forward)
-    # zeeguu.app.logger.debug(backward)
-    # contributions_never_tested = contributions_never_tested.filter_by(card=None)
-
-    contributions_never_tested = forward.union(backward).filter_by(card=None)
-    zeeguu.app.logger.debug(contributions_never_tested)
-
-    if contributions_never_tested.count() > 0:
+    contributions = forward.union(backward).filter_by(card=None)
+    if contributions.count() > 0:
         card = model.Card(
-            contributions_never_tested.
-                join(model.Word, model.Contribution.origin).
-                order_by(model.Word.word_rank,model.Contribution.time).first()
+            contributions.join(model.Word, model.Contribution.origin).order_by(model.Word.word_rank, model.Contribution.time).first()
         )
-        card . set_reason("never shown before"
-                + str(card.contribution.origin.word_rank)
-                + " card position: " + str(card.position))
-
+        card.set_reason("never seen before. " + " rank: " + str(card.contribution.origin.word_rank))
     else:
         cards = (
             model.Card.query.join(model.Contribution, model.Card.contribution)
@@ -159,21 +202,12 @@ def get_next_card(user, from_lang, to_lang):
             model.Word.language == to_lang,
             model.WordAlias.language == from_lang
         )
-        cards = forward.union(backward).filter(model.Card.position < 5)
-        card = cards.order_by(model.Card.last_seen).first()
-        card . set_reason("all were shown before. rank: "
-                          + str(card.contribution.origin.word_rank)
-                          + " card position: " + str(card.position)
-        )
-    return card
 
 
-@gym.route("/gym/question/<from_lang>/<to_lang>")
-def question(from_lang, to_lang):
-    if not flask.g.user:
-        return flask.abort(401)
+        cards = forward.union(backward).filter(model.Card.position < 5).all()
+        card = redisplay_card_aware_of_days(cards)
 
-    card = get_next_card(flask.g.user, from_lang, to_lang)
+
 
     if card is None:
         return "\"NO CARDS\""
@@ -193,8 +227,8 @@ def question(from_lang, to_lang):
         "example":card.contribution.text.content,
         "answer": answer.word,
         "id": card.id,
-        "reason":card.reason,
-        "position": card.position
+        "position": card.position,
+        "reason": card.reason
     })
 
 
