4647ee70553ea2057644c09793fabbe492f84342
Mircea Lungu <mircea.lungu@gmail.com>
2014-05-08 14:35:40 -0400

    small renames and reformatting

--- zeeguu/api/views.py
+++ zeeguu/api/views.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf8 -*-
 import functools
 
@@ -7,7 +6,6 @@ import sqlalchemy.exc
 
 import zeeguu
 from zeeguu import model
-import sys
 
 
 api = flask.Blueprint("api", __name__)
@@ -26,31 +24,35 @@ def with_user(view):
         flask.g.user = session.user
         session.update_use_date()
         return view(*args, **kwargs)
+
     return wrapped_view
+
 
 def with_user_test(view):
     @functools.wraps(view)
     def wrapped_view(*args, **kwargs):
         flask.g.user = model.User.find("user@localhost.com")
-    	return view(*args, **kwargs)
+        return view(*args, **kwargs)
+
     return wrapped_view
 
 
-def crossdomain(view):
+def cross_domain(view):
     @functools.wraps(view)
     def wrapped_view(*args, **kwargs):
         response = flask.make_response(view(*args, **kwargs))
         response.headers['Access-Control-Allow-Origin'] = "*"
         return response
+
     return wrapped_view
 
 
-def decodeWord(word):
+def decode_word(word):
     return word.replace("+", " ")
 
 
 @api.route("/adduser/<email>", methods=["POST"])
-def adduser(email):
+def add_user(email):
     password = flask.request.form.get("password", None)
     if password is None:
         flask.abort(400)
@@ -65,111 +67,111 @@ def adduser(email):
 
 
 @api.route("/session/<email>", methods=["POST"])
-@crossdomain
+@cross_domain
 def get_session(email):
-	password = flask.request.form.get("password", None)
-	if password is None:
-		flask.abort(400)
-	user = model.User.authorize(email, password)
-	if user is None:
-		flask.abort(401)
-	session = model.Session.for_user(user)
-	zeeguu.db.session.add(session)
-	zeeguu.db.session.commit()
-	return str(session.id)
+    password = flask.request.form.get("password", None)
+    if password is None:
+        flask.abort(400)
+    user = model.User.authorize(email, password)
+    if user is None:
+        flask.abort(401)
+    session = model.Session.for_user(user)
+    zeeguu.db.session.add(session)
+    zeeguu.db.session.commit()
+    return str(session.id)
 
 
 @api.route("/contribs", methods=["GET"])
-@crossdomain
+@cross_domain
 @with_user
-def contribs():
-	#at this moment it seems nice to exchange data in json format
-	contribs = flask.g.user.contribs_chronologically()
+def contributions():
+    #at this moment it seems nice to exchange data in json format
+    contributions = flask.g.user.contribs_chronologically()
 
-	words = []
-	for contrib in contribs:
-		word = {}
-		word ['from'] = contrib.origin.word
-		word ['to'] = contrib.translation.word
-		words.append (word)
+    words = []
+    for contrib in contributions:
+        word = {}
+        word['from'] = contrib.origin.word
+        word['to'] = contrib.translation.word
+        words.append(word)
 
-	import json
-	js = json.dumps(words)
-	resp = flask.Response(js, status=200, mimetype='application/json')
-	return resp
+    import json
+
+    js = json.dumps(words)
+    resp = flask.Response(js, status=200, mimetype='application/json')
+    return resp
+
 
 @api.route("/contribs_by_day", methods=["GET"])
-@crossdomain
+@cross_domain
 @with_user
-def contribs_by_day():
-	usr = flask.g.user
-        contribs_by_date,sorted_dates = usr.contribs_by_date()
+def contributions_by_day():
+    usr = flask.g.user
+    contribs_by_date, sorted_dates = usr.contribs_by_date()
 
-	dates = []
-	for date in sorted_dates:
-        	words = []
-        	for contrib in contribs_by_date[date]:
-                	word = {}
-                	word ['from'] = contrib.origin.word
-                	word ['to'] = contrib.translation.word
-                	words.append (word)
-		date_entry = {}
-		date_entry ['date'] = date.strftime("%A, %d %B")
-		date_entry ['contribs'] = words
-		dates.append(date_entry)
+    dates = []
+    for date in sorted_dates:
+        words = []
+        for contrib in contribs_by_date[date]:
+            word = {}
+            word['from'] = contrib.origin.word
+            word['to'] = contrib.translation.word
+            words.append(word)
+        date_entry = {}
+        date_entry['date'] = date.strftime("%A, %d %B")
+        date_entry['contribs'] = words
+        dates.append(date_entry)
 
-        import json
-        js = json.dumps(dates)
-        resp = flask.Response(js, status=200, mimetype='application/json')
-        return resp
+    import json
+
+    js = json.dumps(dates)
+    resp = flask.Response(js, status=200, mimetype='application/json')
+    return resp
 
 
 @api.route("/contribute/<from_lang_code>/<term>/<to_lang_code>/<translation>",
            methods=["POST"])
-@crossdomain
+@cross_domain
 @with_user
 def contribute(from_lang_code, term, to_lang_code, translation):
     from_lang = model.Language.find(from_lang_code)
     to_lang = model.Language.find(to_lang_code)
 
-    word = model.Word.find(decodeWord(term), from_lang)
-    translation = model.Word.find(decodeWord(translation), to_lang)
+    word = model.Word.find(decode_word(term), from_lang)
+    translation = model.Word.find(decode_word(translation), to_lang)
     search = model.Search.query.filter_by(
         user=flask.g.user, word=word, language=to_lang
     ).order_by(model.Search.id.desc()).first()
     import datetime
-    search.contribution = model.Contribution(word, translation, flask.g.user,datetime.datetime.now())
+
+    search.contribution = model.Contribution(word, translation, flask.g.user, datetime.datetime.now())
 
     zeeguu.db.session.commit()
 
     return "OK"
 
+
 @api.route("/contribute_with_context/<from_lang_code>/<term>/<to_lang_code>/<translation>",
            methods=["POST"])
-@crossdomain
+@cross_domain
 @with_user
 def contribute_with_context(from_lang_code, term, to_lang_code, translation):
-
     url = model.Url.find(str(flask.request.form['url']))
     context = flask.request.form['context']
-
-    # sys.stderr.write(term+"\n")
-    # sys.stderr.write(translation+"\n")
-    # sys.stderr.write("url:" + url + "\n")
-    # sys.stderr.write("context:" + context)
 
     from_lang = model.Language.find(from_lang_code)
     to_lang = model.Language.find(to_lang_code)
 
-    word = model.Word.find(decodeWord(term), from_lang)
-    translation = model.Word.find(decodeWord(translation), to_lang)
+    word = model.Word.find(decode_word(term), from_lang)
+    translation = model.Word.find(decode_word(translation), to_lang)
     search = model.Search.query.filter_by(
         user=flask.g.user, word=word, language=to_lang
     ).order_by(model.Search.id.desc()).first()
 
     #create the text entity first
-    new_text = model.Text(context,from_lang, url)
+    new_text = model.Text(context, from_lang, url)
     import datetime
+
     search.contribution = model.Contribution(word, translation, flask.g.user, new_text, datetime.datetime.now())
 
     zeeguu.db.session.commit()
@@ -178,7 +180,7 @@ def contribute_with_context(from_lang_code, term, to_lang_code, translation):
 
 
 @api.route("/lookup/<from_lang>/<term>/<to_lang>", methods=("POST",))
-@crossdomain
+@cross_domain
 @with_user
 def lookup(from_lang, term, to_lang):
     from_lang = model.Language.find(from_lang)
@@ -192,7 +194,7 @@ def lookup(from_lang, term, to_lang):
     else:
         text = None
     user.searches.append(
-        model.Search(user, model.Word.find(decodeWord(term), from_lang),
+        model.Search(user, model.Word.find(decode_word(term), from_lang),
                      to_lang, text)
     )
     zeeguu.db.session.commit()
@@ -200,14 +202,14 @@ def lookup(from_lang, term, to_lang):
 
 
 @api.route("/lookup/<from_lang>/<term>", methods=("POST",))
-@crossdomain
+@cross_domain
 @with_user
 def lookup_preferred(from_lang, term):
     return lookup(from_lang, term, flask.g.user.preferred_language)
 
 
 @api.route("/validate")
-@crossdomain
+@cross_domain
 @with_user
 def validate():
     return "OK"
