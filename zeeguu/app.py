409a2b7379e11e7c4cda10536bcdd7c7456dd641
Mircea Lungu <mircea.lungu@gmail.com>
2014-05-08 15:05:01 -0400

    bugfix

--- zeeguu/app.py
+++ zeeguu/app.py
@@ -7,6 +7,7 @@ import flask
 import zeeguu
 import zeeguu.gym.views
 import zeeguu.api.views
+import zeeguu.account.views
 
 
 class CrossDomainApp(flask.Flask):
@@ -17,15 +18,15 @@ class CrossDomainApp(flask.Flask):
         rv.headers['Access-Control-Allow-Origin'] = "*"
         return rv
 
-    # create the instance folder and return the path
-    def instance_path(self):
-        path = os.path.join(self.instance_path, "gen")
-        try:
-            os.makedirs(path)
-        except:
-            if not os.path.isdir(path):
-                raise
-        return path
+# create the instance folder and return the path
+def instance_path(app):
+    path = os.path.join(app.instance_path, "gen")
+    try:
+        os.makedirs(path)
+    except:
+        if not os.path.isdir(path):
+            raise
+    return path
 
 # *** Starting the App *** #
 app = CrossDomainApp(__name__, instance_relative_config=True)
@@ -33,9 +34,10 @@ app = CrossDomainApp(__name__, instance_relative_config=True)
 app.config.from_object("zeeguu.default_config")
 app.config.from_pyfile("config.cfg", silent=True) #config.cfg is in the instance folder
 
-instance = flask.Blueprint("instance", __name__, static_folder=app.instance_path)
+instance = flask.Blueprint("instance", __name__, static_folder=instance_path(app))
 
 app.register_blueprint(instance)
 app.register_blueprint(zeeguu.gym.views.gym)
 app.register_blueprint(zeeguu.api.views.api)
+app.register_blueprint(zeeguu.account.views.account)
 
