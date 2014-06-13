230c90a86004ba2d29c7390558419aae80e4d676
Mircea Lungu <mircea.lungu@gmail.com>
2014-06-13 19:21:28 +0200

    test db is not a file anymore

--- tests.py
+++ tests.py
@@ -8,14 +8,15 @@ class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
     	print "test setup"
-        self.db_fd, zeeguu.app.config['DATABASE'] = tempfile.mkstemp()
+        # self.db_fd, zeeguu.app.config['DATABASE'] = tempfile.mkstemp()
         zeeguu.app.config['TESTING'] = True
         self.app = zeeguu.app.test_client()
         zeeguu.populate.create_test_db()
 
     def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(zeeguu.app.config['DATABASE'])
+        # os.close(self.db_fd)
+        # os.unlink(zeeguu.app.config['DATABASE'])
+        pass
 
     def test_homepage_rendered_correctly(self):
 	    rv = self.app.get('/')
