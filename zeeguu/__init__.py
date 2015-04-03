ac33c38c4a2de459543c973e871f8f83b0364d8c
Mircea Lungu <mircea.lungu@gmail.com>
2015-04-04 00:53:09 +0200

    on OS X the connection must be made to "localhost"

--- zeeguu/__init__.py
+++ zeeguu/__init__.py
@@ -16,7 +16,10 @@ app = app.app
 
 if os.environ.get("ZEEGUU_TESTING") == "True":
     app.config.pop("SQLALCHEMY_DATABASE_URI", None)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://zeeguu_test:zeeguu_test@127.0.0.1/zeeguu_test"
+    if os.uname()[0]=='Darwin':
+        app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://zeeguu_test:zeeguu_test@localhost/zeeguu_test"
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://zeeguu_test:zeeguu_test@127.0.0.1/zeeguu_test"
     print "[ Using the test DB (" + app.config["SQLALCHEMY_DATABASE_URI"] + ") ]"
 
 
