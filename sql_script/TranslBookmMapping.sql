862fef2ff7a86ef38589b3c9f3d3b450ff958e6f
karans13 <karan.sethi@students.unibe.ch>
2015-04-14 11:56:36 +0200

    updated sqlscript

--- sql_script/TranslBookmMapping.sql
+++ sql_script/TranslBookmMapping.sql
@@ -1,11 +1,17 @@
+DROP TABLE IF EXISTS bookmark_translation_mapping;
 
-RENAME TABLE exercise_log TO exercise;
 
-RENAME TABLE exercise_log_source TO exercise_source;
+RENAME TABLE contribution TO bookmark;
 
-RENAME TABLE exercise_log_outcome TO exercise_outcome;
+RENAME TABLE contribution_exercise_mapping TO bookmark_exercise_mapping;
 
-RENAME TABLE bookmark_exercise_log_mapping TO bookmark_exercise_mapping
+CREATE TABLE bookmark_translation_mapping
+(
+bookmark_id INT NOT NULL,
+translation_id INT NOT NULL,
+FOREIGN KEY (bookmark_id) REFERENCES bookmark(id),
+FOREIGN KEY (translation_id) REFERENCES word(id)
+)
 
 
 
