0aca061b8a6f0ca30bf6f17b4343e73a82fe31c0
Mircea Lungu <mircea.lungu@gmail.com>
2015-05-13 18:31:27 +0200

    bug fix in update_ranks_in_user_wr4ods

--- sql_script/util__update_ranks_in_user_words.sql
+++ sql_script/util__update_ranks_in_user_words.sql
@@ -3,12 +3,12 @@ SET SQL_SAFE_UPDATES=0;
 update user_words t1
 inner JOIN
 	(
-	select uw.id user_words_id, wr.rank word_rank_rank, uw.language_id
+	select uw.id user_words_id, wr.id word_rank_id, uw.language_id
 		from user_words uw
 		join word_ranks wr
 		on lower(uw.word) = lower(wr.word) and uw.language_id = wr.language_id
 		) t2
 	on t1.id = t2.user_words_id
-	set t1.`rank_id` = t2.word_rank_rank;
+	set t1.`rank_id` = t2.word_rank_id;
 
 SET SQL_SAFE_UPDATES=1;
