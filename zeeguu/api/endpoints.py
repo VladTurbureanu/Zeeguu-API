281df5bb75a51178d89bfd5a31d6b4bd59f41f65
karans13 <karan.sethi@students.unibe.ch>
2015-09-03 20:07:15 +0200

    changed metric language known to confidence interval language known

--- zeeguu/api/endpoints.py
+++ zeeguu/api/endpoints.py
@@ -499,11 +499,17 @@ def get_probably_known_words(lang_code):
     resp = flask.Response(js, status=200, mimetype='application/json')
     return resp
 
-@api.route("/get_percentage_of_language_known", methods=("GET",))
+@api.route("/get_lower_bound_percentage_of_language_known", methods=("GET",))
 @cross_domain
 @with_session
-def get_percentage_of_language_known():
-    return flask.g.user.get_percentage_of_language_known()
+def get_lower_bound_percentage_of_language_known():
+    return flask.g.user.get_lower_bound_percentage_of_language_known()
+
+@api.route("/get_upper_bound_percentage_of_language_known", methods=("GET",))
+@cross_domain
+@with_session
+def get_upper_bound_percentage_of_language_known():
+    return flask.g.user.get_upper_bound_percentage_of_language_known()
 
 # returns the percentage of how many bookmarks are known to the user out of all the bookmarks
 @api.route("/get_percentage_of_probably_known_bookmarked_words", methods=("GET",))
