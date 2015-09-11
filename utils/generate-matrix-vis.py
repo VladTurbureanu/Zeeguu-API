493f4e376448203264444f57fef60fade052b21c
Mircea Lungu <mircea.lungu@gmail.com>
2015-09-11 10:54:06 +0200

    visualizing words...

--- utils/generate-matrix-vis.py
+++ utils/generate-matrix-vis.py
@@ -1,19 +1,20 @@
 import numpy
 import matplotlib.pylab as plt
 
-def generate_matrix_visualization(known_words):
+def generate_matrix_visualization(known_words, learning_words):
 
 	m = []
 	for i in range(0,100):
 		m.append([])
 		for j in range (0, 100):
 			if (i*100+j) in known_words:
-				m[i].append(1)
+				m[i].append(0.1)
+			elif (i*100+j) in learning_words: 
+				m[i].append(0.6)
 			else:
-				m[i].append(0.65)
+				m[i].append(1)
 	
 	# we need this next line to scale the color scheme
-	m[0][0]=0
 
 	matrix = numpy.matrix(m)
 
@@ -22,15 +23,20 @@ def generate_matrix_visualization(known_words):
 	ax = fig.add_subplot(1,1,1)
 	ax.set_aspect('equal')
 	plt.imshow(matrix, interpolation='none')
+	plt.set_cmap('hot')
 	plt.colorbar()
 	plt.show()
 
 
 # assume known words in current folder words.txt file
-known_words = []
-
+known_words = [ ]
+learning_words = [ ]
 with open ('words.txt') as f:
 	for line in f: 
 		known_words.append(int	(line))
 
-generate_matrix_visualization(known_words)
+with open ('learning.txt') as f:
+	for line in f: 
+		learning_words.append(int	(line))
+
+generate_matrix_visualization(known_words, learning_words)
