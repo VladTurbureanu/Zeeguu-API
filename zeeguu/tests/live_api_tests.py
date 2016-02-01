696f2e1a56089018bdc9a9ec9af79260f73d939a
Mircea Lungu <mircea.lungu@gmail.com>
2016-02-02 00:15:02 +0100

    get contents from url

--- zeeguu/tests/live_api_tests.py
+++ zeeguu/tests/live_api_tests.py
@@ -1,9 +1,11 @@
 import unittest
 import requests
+import json
 import zeeguu
 
 USER=zeeguu.app.config.get("TEST_USER")
 PASS=zeeguu.app.config.get("TEST_PASS")
+ZSRV="https://zeeguu.unibe.ch/"
 
 class Live_API_Tests(unittest.TestCase):
 
@@ -23,5 +25,36 @@ class Live_API_Tests(unittest.TestCase):
             password=PASS
         ))
         assert rv.text > 0
+        return rv.text
+
+
+    def in_session(self, session, url, other_args=[]):
+        print session
+        print url
+        url_with_session = url + "?session=" + session
+
+        for each in other_args:
+            url_with_session += "&" + each
+        print url_with_session
+        return url_with_session
+
+    def test_content_from_url(self):
+        manual_check = False
+
+        data = dict(urls=[
+                    dict(url="http://www.derbund.ch/wirtschaft/unternehmen-und-konjunktur/die-bankenriesen-in-den-bergkantonen/story/26984250",
+                         id=1)])
+
+        r = requests.post(self.in_session(self.test_get_session(), ZSRV+'get_content_from_url'),
+                          json=data)
+
+        urls = r.json()['contents']
+        for url in urls:
+            assert url['content'] is not None
+            assert url['image'] is not None
+            if manual_check:
+                print url['content']
+                print url['image']
+
 
 
