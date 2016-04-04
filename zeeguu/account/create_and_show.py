201be95cb227301937e0c26ca4a5609f2a145b9f
Mircea Lungu <mircea.lungu@gmail.com>
2016-04-04 23:25:30 +0200

    update for ancu's friend in mph

--- zeeguu/account/create_and_show.py
+++ zeeguu/account/create_and_show.py
@@ -38,7 +38,7 @@ def create_account():
     language = Language.find(form.get("language", None))
     native_language = Language.find(form.get("native_language", None))
 
-    if not (code == "Kairo" or code == "unibe" or code == "rug"):
+    if not (code == "Kairo" or code == "unibe" or code == "rug" or code =="42"):
         flash("Invitation code is not recognized. Please contact us.")
 
     elif password is None or email is None or name is None:
